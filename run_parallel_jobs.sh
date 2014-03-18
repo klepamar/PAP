47580ea328160bbd23cd0123d3386fbf170d24a6
Martin Klepac <klepamar@star.star>
2014-03-18 19:27:06 +0100

    Drobe zmeny na skriptoch tykajuce sa prace na star-e

--- run_parallel_jobs.sh
+++ run_parallel_jobs.sh
@@ -2,13 +2,17 @@
 
 # vystup - spustenie paralelnych jobov vygenerovanych skriptom create_parallel_jobs.sh
 
-# make sure correct method is being invoked in index.cpp - change index.cpp & run make afterwards
-
-for INPUT_SIZE in 1024 2048 4096
+for I in 1 2 3 # repeat 3 times for more precise results
 do
-	for THREAD_NO in 1 2 4 6 8 12
+	for PROGRAM in classic optimised # add strassen64 when parallel implementation is ready
 	do
-		/opt/bin/qrun.sh 24c 1 1slots_per_host parallel_job_${INPUT_SIZE}_${THREAD_NO}.sh
+		for INPUT_SIZE in 1024 2048 4096 # for all available inputs
+		do
+			for THREAD_NO in 1 2 4 6 8 12 24 # for given number of threads
+			do
+				/opt/bin/qrun.sh 24c 1 1slots_per_host parallel_job_${PROGRAM}_${INPUT_SIZE}_${THREAD_NO}.sh
+			done
+		done
 	done
 done
 
